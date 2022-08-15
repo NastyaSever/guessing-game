1c208a176201194b8b0c9f4f1812e1f5497c50f3
Nastya Severina <Severina.Nastya1992@yandex.by>
2022-08-15 22:52:41 +0300

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return Math.round((this.minValue + this.maxValue)/2) 
     }
 
     lower() {
-
+        this.maxValue = Math.round((this.maxValue + this.minValue)/2)
     }
 
     greater() {
-
+        this.minValue = Math.round((this.maxValue + this.minValue)/2)
     }
 }
 
